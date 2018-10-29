a96e35044cca31021bd23fd36b9d1bb6586c9353
simontruong2@gmail.com <simontruong2@gmail.com>
2018-10-29 16:38:20 +1100

    Added Users controller with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'welcome#index'
   get 'mentors' => 'mentors#index'
   get 'mentors/:id' => 'mentors#show'
+  get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
