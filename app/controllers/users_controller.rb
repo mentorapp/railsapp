42a146df8214b1eebfe682a5f00e9b95d831ab87
Vanessa Nimmo <vanessanimmo@live.com>
2018-10-31 15:30:01 +1100

    Added user info to user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(current_user.id)
+    @bookings = Booking.all
+    @mentor_bookings = []
+    @mentee_bookings = []
+    @bookings.each do |booking|
+      if booking.mentor_id == current_user.id
+        @mentor_bookings << booking
+      elsif booking.mentee_id == current_user.id
+        @mentee_bookings << booking
+      end
+    end
   end
 end
