ef9b366c5deaec267454f9471e661633443dd74d
Vanessa Nimmo <vanessanimmo@live.com>
2018-10-31 16:44:44 +1100

    Mentor edit form added

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,23 +1,43 @@
 class MentorsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_mentor, only: [:show, :edit, :update, :destroy]
 
   def index
     @mentors = Mentor.all
   end
 
   def show
-    @mentor = Mentor.find(params[:id])
+  end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def edit
   end
 
   def create
   end
 
+  def update
+    @mentor.update(mentor_params)
+    redirect_to users_show_path
+  end
+
+  def destroy
+    @mentor.destroy
+  end
+
   private
 
+  def set_mentor
+    @mentor = Mentor.find(params[:id])
+  end
   # Takes the user_id of the user who elected to become a mentor and uses this as a foreign key in the mentors table to connect a mentor profile with a user.
   def mentor_params
     result = params.require(:mentor).permit(:user_id, :skills, :availability, :price)
     result[:user_id] = current_user.id
+    result[:price] = result[:price].to_f * 100.0
     result
   end
 end
