78b3da21213f38f316735ee13e67d4437eb8e140
simontruong2@gmail.com <simontruong2@gmail.com>
2018-11-01 16:15:30 +1100

    Fixed redirection of create and update actions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,53 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    @user = User.create!(sign_up_params)
-    session[:user_id] = @user.id
-    # If the user selects the 'Mentor' checkbox this param will be equal to 1 and this if statement will call create on the Mentor model.
-    if params[:mentor] == "1"
-      Mentor.create({user_id: @user.id})
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        if params[:mentor] == "1"
+          Mentor.create({user_id: resource.id})
+        end
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        redirect_to mentors_path
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
-    redirect_to root_path
   end
+
+  def update
+    if params[:mentor] == '1'
+      Mentor.create({user_id: current_user.id})
+    end
+    
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      redirect_to users_show_path
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
 end
