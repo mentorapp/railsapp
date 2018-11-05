24360b5974b61659db5b146ed9c69194585f4485
simontruong2@gmail.com <simontruong2@gmail.com>
2018-11-05 15:50:53 +1100

    Added bookings new and show page and before_action in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-    
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
   # GET /products
   # GET /products.json
   def index
@@ -70,7 +70,7 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      result = params.require(:booking).permit(:mentor_id, :mentee_id, :price, :charge_id)
+      result = params.require(:booking).permit(:mentor_id, :mentee_id, :price, :charge_id, :start_time)
       result[:price] = result[:price].to_f * 100.0
       result
     end
