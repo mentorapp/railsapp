5e552948edbfef8e73144d27aa967a761dfb0b55
Corey Hellwege <coreyhellwege@gmail.com>
2018-11-05 16:15:48 +1100

    rolled back last migration to fix database conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_002723) do
+ActiveRecord::Schema.define(version: 2018_11_02_010334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 2018_11_05_002723) do
     t.datetime "updated_at", null: false
     t.string "stripe_user_id"
     t.index ["user_id"], name: "index_mentors_on_user_id"
-  end
-
-  create_table "searches", force: :cascade do |t|
-    t.string "keywords"
-    t.decimal "min_price"
-    t.decimal "max_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
