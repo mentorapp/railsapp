8ae48e5dc4bcefcf58b75d6bb1ee0950cfaa4798
Corey Hellwege <coreyhellwege@gmail.com>
2018-11-05 16:20:58 +1100

    created migration for searches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_043501) do
+ActiveRecord::Schema.define(version: 2018_11_05_051904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 2018_11_05_043501) do
     t.datetime "updated_at", null: false
     t.index ["mentor_id"], name: "index_reviews_on_mentor_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "keywords"
+    t.decimal "min_price"
+    t.decimal "max_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
