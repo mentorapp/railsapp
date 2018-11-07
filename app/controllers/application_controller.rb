a940bc56f90e99b57f63304bfe5cbf4397bef2d2
simontruong2@gmail.com <simontruong2@gmail.com>
2018-11-07 14:41:15 +1100

    Added checkbox to verify age and terms and services

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # Permit the about_me, purpose, avatar, email, password, username and mentor parameters on signup.
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password_confirmation, :about_me, :purpose, :avatar, :email, :password, :username, :mentor])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password_confirmation, :about_me, :purpose, :avatar, :email, :password, :username, :mentor, :ageconfirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:about_me, :purpose, :avatar, :username, :email, :password, :password_confirmation, :mentor])
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,31 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    build_resource(sign_up_params)
+    if params[:ageconfirmation] == '1'
+      build_resource(sign_up_params)
 
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        if params[:mentor] == "1"
-          Mentor.create({user_id: resource.id, price: 0})
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          if params[:mentor] == "1"
+            Mentor.create({user_id: resource.id, price: 0})
+          end
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          redirect_to mentors_path
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
         end
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        redirect_to mentors_path
       else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
       end
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      redirect_to new_user_registration_path, notice: 'Terms and conditions must be accepted'
     end
   end
 
